2fddd94eb76ad150de091d8f29da3e61f4105ea6
Jose Jimenez <josejimenez@Unknown-3c-15-c2-c9-11-90.home>
2017-12-18 21:06:58 +0000

    added coordinates of hidden diamonds

--- MyAdventures/jljc/game.py
+++ MyAdventures/jljc/game.py
@@ -14,6 +14,8 @@ from jljc.printer_3d.scan_print_3d import ScanPrint3D
 mc = minecraft.Minecraft.create()
 scanner = ScanPrint3D(mc)
 
+NUMBER_OF_SECONDS = 15 * 60
+NUMBER_OF_DIAMONDS = 7
 BLOCK_ID = block.DIAMOND_BLOCK.id
 BASE_PATH = os.path.dirname(__file__)
 RESOURCES_PATH = os.path.join(BASE_PATH, '..', 'resources', 'scans', 'game')
@@ -30,11 +32,58 @@ for d in DATA_FILES:
 POTENTIAL_HIDDEN_DIAMONDS = {
     '1': {'where': 'village', 'coord': Vec3(20, 0, 56)},
     '2': {'where': 'village', 'coord': Vec3(1, 0, 78)},
+    '3': {'where': 'village', 'coord': Vec3(-18, 1, 52)},
+    '4': {'where': 'village', 'coord': Vec3(-40, 10, 39)},
+    '5': {'where': 'village', 'coord': Vec3(-13, 5, 42)},
+    '6': {'where': 'village', 'coord': Vec3(-24, 1, 37)},
+    '7': {'where': 'forest', 'coord': Vec3(-70, 0, 18)},
+    '8': {'where': 'forest', 'coord': Vec3(-100, 13, 19)},
+    '9': {'where': 'forest', 'coord': Vec3(-90, 0, 83)},
+    '10': {'where': 'talia_mansion', 'coord': Vec3(-107, 13, 144)},
+    '11': {'where': 'talia_mansion', 'coord': Vec3(-126, 13, 148)},
+    '12': {'where': 'talia_mansion', 'coord': Vec3(-121, 9, 150)},
+    '13': {'where': 'talia_mansion', 'coord': Vec3(-113, 9, 137)},
+    '14': {'where': 'talia_mansion', 'coord': Vec3(-129, 9, 138)},
+    '15': {'where': 'talia_mansion', 'coord': Vec3(-118, 9, 151)},
+    '16': {'where': 'talia_mansion', 'coord': Vec3(-120, 6, 137)},
+    '17': {'where': 'talia_mansion', 'coord': Vec3(-129, 5, 135)},
+    '18': {'where': 'talia_mansion', 'coord': Vec3(-129, 5, 144)},
+    '19': {'where': 'talia_mansion', 'coord': Vec3(-123, 0, 143)},
+    '20': {'where': 'talia_mansion', 'coord': Vec3(-107, 1, 144)},
+    '21': {'where': 'forest', 'coord': Vec3(-130, 0, 193)},
+    '22': {'where': 'forest', 'coord': Vec3(-127, 11, 206)},
+    '23': {'where': 'selina_mansion', 'coord': Vec3(-115, 18, 249)},
+    '24': {'where': 'selina_mansion', 'coord': Vec3(-129, 13, 236)},
+    '25': {'where': 'selina_mansion', 'coord': Vec3(-116, 14, 254)},
+    '26': {'where': 'selina_mansion', 'coord': Vec3(-113, 13, 254)},
+    '27': {'where': 'selina_mansion', 'coord': Vec3(-119, 13, 254)},
+    '28': {'where': 'selina_mansion', 'coord': Vec3(-109, 9, 244)},
+    '29': {'where': 'selina_mansion', 'coord': Vec3(-128, 9, 235)},
+    '30': {'where': 'selina_mansion', 'coord': Vec3(-128, 9, 243)},
+    '31': {'where': 'selina_mansion', 'coord': Vec3(-126, 9, 250)},
+    '32': {'where': 'selina_mansion', 'coord': Vec3(-106, 9, 254)},
+    '33': {'where': 'selina_mansion', 'coord': Vec3(-106, 7, 246)},
+    '34': {'where': 'selina_mansion', 'coord': Vec3(-123, 5, 244)},
+    '35': {'where': 'selina_mansion', 'coord': Vec3(-111, 2, 254)},
+    '36': {'where': 'selina_mansion', 'coord': Vec3(-139, 2, 245)},
+    '37': {'where': 'forest', 'coord': Vec3(-22, 0, 307)},
+    '38': {'where': 'forest', 'coord': Vec3(20, 0, 275)},
+    '39': {'where': 'pond', 'coord': Vec3(0, 0, 241)},
+    '40': {'where': 'pond', 'coord': Vec3(13, 0, 222)},
+    '41': {'where': 'pond', 'coord': Vec3(-4, -1, 209)},
+    '42': {'where': 'pond', 'coord': Vec3(-41, 0, 178)},
+
 }
 
 
-def choose_hidden_diamonds(n=2):
-    return POTENTIAL_HIDDEN_DIAMONDS
+def choose_hidden_diamonds(n=NUMBER_OF_DIAMONDS):
+    chosen = {}
+    ids = POTENTIAL_HIDDEN_DIAMONDS.keys()
+    while len(chosen) < n:
+        id = random.choice(ids)
+        if id not in chosen:
+            chosen[id] = POTENTIAL_HIDDEN_DIAMONDS[id]
+    return chosen
 
 
 def place_hidden_diamonds(data):
@@ -140,7 +189,7 @@ def diamond_quest_was_successful(diamonds, no_seconds=30):
 
 def main():
     """
-    
+
     TODO
 
     the data within DATA_FILE should be level (e.g. 'village', 'selina_mansion', 'explorer', etc
@@ -154,7 +203,7 @@ def main():
     place_hidden_diamonds(diamonds)
 
     mc.postToChat("Let's search for the diamonds, the clock is ticking...")
-    all_found = diamond_quest_was_successful(diamonds, 60)
+    all_found = diamond_quest_was_successful(diamonds, NUMBER_OF_SECONDS)
     if all_found:
         mc.postToChat("Well done - the city is saved - good-bye aliens...")
         what_to_explode = DATA['spaceship_fleet']
